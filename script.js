062e4b2781dad510ff8904100c6d5947a8cdbf1f
samaramorgan-skype <s00927963@gmail.com>
2024-10-20 22:42:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Proxy URL to bypass CORS policy
     const proxyURL = 'https://api.allorigins.win/get?url=';
-    const playlistURL = 'https://github.com/samaramorgan-skype/asdsa2131/blob/main/rus.m3u?raw=true';
+    const playlistURL = 'https://github.com/samaramorgan-skype/asdsa2131/blob/main/russia.m3u?raw=true';
     const proxyPlaylistURL = proxyURL + encodeURIComponent(playlistURL);
 
     fetch(proxyPlaylistURL)
